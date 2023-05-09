e6613b1316ba22977051de57038d273f65e7d959
Valentin Arthur Thomas <warquis958@gmail.com>
2023-05-09 03:27:04 +0200

    correction

--- RespawnTimer/RepsawnTimerPlugin.cs
+++ RespawnTimer/RepsawnTimerPlugin.cs
@@ -1,4 +1,6 @@
-﻿using MEC;
+﻿using System;
+using System.Collections.Generic;
+using MEC;
 using Neuron.Core.Dev;
 using Neuron.Core.Events;
 using Neuron.Core.Meta;
@@ -11,8 +13,6 @@ using Synapse3.SynapseModule.Events;
 using Synapse3.SynapseModule.Map;
 using Synapse3.SynapseModule.Player;
 using Synapse3.SynapseModule.Teams;
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace RespawnTimer;
@@ -23,7 +23,7 @@ namespace RespawnTimer;
     Version = "1.0.0",
     Author = "VT"
 )]
-public class RepsawnTimerPlugin : ReloadablePlugin<RepsawnTimerConfig, RespawnTimerTranslations>
+public class RespawnTimerPlugin : ReloadablePlugin<RespawnTimerConfig, RespawnTimerTranslations>
 {
     public const string DataKey = "RespawnTimer";
 
@@ -32,13 +32,13 @@ public class RepsawnTimerPlugin : ReloadablePlugin<RepsawnTimerConfig, RespawnTi
 [Automatic]
 public class ExampleEventHandler : Listener
 {
-    private readonly RepsawnTimerPlugin _plugin;
+    private readonly RespawnTimerPlugin _plugin;
     private readonly RoundService _round;
     private readonly PlayerService _player;
     private readonly TeamService _team;
 
 
-    public ExampleEventHandler(RepsawnTimerPlugin plugin
+    public ExampleEventHandler(RespawnTimerPlugin plugin
         , RoundService round, PlayerService player, TeamService team)
     {
         _plugin = plugin;
@@ -52,20 +52,20 @@ public class ExampleEventHandler : Listener
     public void Join(JoinEvent ev)
     {
         bool value;
-        var dataBaseValue = ev.Player.GetData(RepsawnTimerPlugin.DataKey);
+        var dataBaseValue = ev.Player.GetData(RespawnTimerPlugin.DataKey);
         if (!bool.TryParse(dataBaseValue, out value))
         {
             value = true;
-            ev.Player.SetData(RepsawnTimerPlugin.DataKey, true.ToString());
+            ev.Player.SetData(RespawnTimerPlugin.DataKey, true.ToString());
         }
-        ev.Player.Data[RepsawnTimerPlugin.DataKey] = value;
+        ev.Player.Data[RespawnTimerPlugin.DataKey] = value;
     }
 
     [EventHandler]
     public void Leave(LeaveEvent ev)
     {
-        var value = ev.Player.Data[RepsawnTimerPlugin.DataKey];
-        ev.Player.SetData(RepsawnTimerPlugin.DataKey, value.ToString());
+        var value = ev.Player.Data[RespawnTimerPlugin.DataKey];
+        ev.Player.SetData(RespawnTimerPlugin.DataKey, value.ToString());
     }
 
     [EventHandler]
@@ -86,19 +86,19 @@ public class ExampleEventHandler : Listener
                 || p.RoleType == RoleTypeId.Overwatch), PlayerType.Player);
             foreach (var player in players)
             {
-                if ((bool)player.Data[RepsawnTimerPlugin.DataKey])
+                if ((bool)player.Data[RespawnTimerPlugin.DataKey])
                     continue;
 
-                var mesasge = player.GetTranslation(translations).RespawnMessage;
-                mesasge = mesasge.Replace("\\n", "\n");
+                var message = player.GetTranslation(translations).RespawnMessage;
+                message = message.Replace("\\n", "\n");
                 var time = TimeSpan.FromSeconds(RespawnManager.Singleton._timeForNextSequence - RespawnManager.Singleton._stopwatch.Elapsed.TotalSeconds);
                 var timeTeam = _team.NextTeam == uint.MaxValue 
                     ? time.ToString("mm\\:ss") 
                     : $"({_team.GetTeamName(_team.NextTeam)}) {time.ToString("mm\\:ss")}";
                 var mtfTicket = Mathf.Round(RespawnTokensManager.Counters[1].Amount);
                 var chiTicket = Mathf.Round(RespawnTokensManager.Counters[0].Amount);
-                mesasge = mesasge.Format(timeTeam, mtfTicket, chiTicket);
-                player.SendHint(mesasge, 1.25f);
+                message = message.Format(timeTeam, mtfTicket, chiTicket);
+                player.SendHint(message, 1.25f);
             }
 
         }
